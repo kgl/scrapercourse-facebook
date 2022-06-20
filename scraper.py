cf7a9785021c568cbe3fb551c961a0c8ec87280a
kgl <john.tw@gmail.com>
2022-06-20 11:55:30 +0800

    6.6批次爬取登入型網頁的資料

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 import credentials
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 
 options = Options()
@@ -28,3 +29,16 @@ driver.get("https://www.facebook.com/groups/235385084142732")
 for i in range(20):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(3)
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+posts = soup.find_all("div", {
+    "class": "du4w35lb l9j0dhe7"})
+
+result = []
+for post in posts:
+    content = post.find("span", {
+                        "class": "d2edcug0 hpfvmrgz qv66sw1b c1et5uql oi732d6d ik7dh3pa ht8s03o8 a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d9wwppkn iv3no6db jq4qci2q a3bd9o3v b1v8xokw oo9gr5id hzawbc8m"})
+    result.append(content.getText())
+
+print(result)
